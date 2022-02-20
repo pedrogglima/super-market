a337a334f3a5f99db02c6eaae9bdb4215280a414
Pedro Lima <pedrogglima@gmail.com>
2022-02-20 20:44:31 -0300

    remove unecessary money format

--- super_market/checkout_decorator.rb
+++ super_market/checkout_decorator.rb
@@ -5,15 +5,15 @@ require 'money'
 module SuperMarket
   module CheckoutDecorator
     def total
-      Money.from_amount(total_amount.to_f.round(2), 'GBP').format
+      Money.from_amount(total_amount, 'GBP').format
     end
 
     def total_discount
-      Money.from_amount(total_discount_amount.to_f.round(2), 'GBP').format
+      Money.from_amount(total_discount_amount, 'GBP').format
     end
 
     def total_before_discount
-      Money.from_amount(total_before_discount_amount.to_f.round(2), 'GBP').format
+      Money.from_amount(total_before_discount_amount, 'GBP').format
     end
   end
 end
